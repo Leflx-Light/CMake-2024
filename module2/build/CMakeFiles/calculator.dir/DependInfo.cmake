adb901e3d0de0469aa58c07546739e97143111cc
Sonu <leflxlight@gmail.com>
2024-08-21 15:18:19 +0530

    module 2

---
+++ module2/build/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kpit/Desktop/cmake_tutorials/module2/addition.cpp" "/home/kpit/Desktop/cmake_tutorials/module2/build/CMakeFiles/calculator.dir/addition.cpp.o"
+  "/home/kpit/Desktop/cmake_tutorials/module2/division.cpp" "/home/kpit/Desktop/cmake_tutorials/module2/build/CMakeFiles/calculator.dir/division.cpp.o"
+  "/home/kpit/Desktop/cmake_tutorials/module2/main.cpp" "/home/kpit/Desktop/cmake_tutorials/module2/build/CMakeFiles/calculator.dir/main.cpp.o"
+  "/home/kpit/Desktop/cmake_tutorials/module2/print_result.cpp" "/home/kpit/Desktop/cmake_tutorials/module2/build/CMakeFiles/calculator.dir/print_result.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
